d00e27a1eabef0e2a53133d20e6e23339e84825c
brandcwh22 <brandcwh22@gmail.com>
2021-10-29 10:48:11 +0800

    Dice Race Game

--- Dice Race Game/Dice.java
+++
@@ -1,6 +0,0 @@
-public class Dice {
-    public static int diceRoll (){
-        int diceRoll = (int)(Math.random()*6)+1;
-        return diceRoll;
-    }
-}
--- Dice Race Game/GameTest.java
+++
@@ -1,7 +0,0 @@
-public class GameTest {
-
-    public static void main(String[] args) {
-        GameMenu startgame = new GameMenu();
-        startgame.startGame(); //starts the game
-    }
-}
